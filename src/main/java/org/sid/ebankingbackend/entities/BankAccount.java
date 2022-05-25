0be0c82a8130bde227620f4e63fd93020c22f9c3
abderral <at.gfi.abderrahim@sesam-vitale.fr>
2022-05-25 12:06:47 +0200

    create entities

--- src/main/java/org/sid/ebankingbackend/entities/BankAccount.java
+++ src/main/java/org/sid/ebankingbackend/entities/BankAccount.java
@@ -5,13 +5,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.sid.ebankingbackend.enums.AccountStatus;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "TYPE", length = 4, discriminatorType = DiscriminatorType.STRING)
 @Data @NoArgsConstructor @AllArgsConstructor
 public class BankAccount {
     @Id
